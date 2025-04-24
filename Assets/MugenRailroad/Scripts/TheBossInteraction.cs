5c8b3a489ab8037ddcbd4c49c0b2edfce60d31e0
Dani <daniofer0@gmail.com>
2025-04-24 11:45:28 +0200

    added basic train appear animation

--- Assets/MugenRailroad/Scripts/TheBossInteraction.cs
+++ Assets/MugenRailroad/Scripts/TheBossInteraction.cs
@@ -1,11 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TheBossInteraction : MonoBehaviour
 {
+    private Animator trainAnimator;
+
+    private void Awake()
+    {
+        trainAnimator = GameObject.Find("BeginMissionTrain").GetComponent<Animator>();
+    }
+    
     public void StartGame()
     {
+        trainAnimator.SetTrigger("Enable");
+        StartCoroutine(EsperarFinAnimacionTren());
+    }
+
+    private IEnumerator EsperarFinAnimacionTren()
+    {
+        AnimatorStateInfo stateInfo = trainAnimator.GetCurrentAnimatorStateInfo(0);
+        while (!stateInfo.IsName("End"))
+        {
+            yield return null;
+            stateInfo = trainAnimator.GetCurrentAnimatorStateInfo(0);
+        }
+        
         GameManager.Instance.StartWagonMission();
     }
 }
