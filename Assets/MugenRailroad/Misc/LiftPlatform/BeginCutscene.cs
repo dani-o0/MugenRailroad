47c33a655631795ed61e5238df29e5da5b45730c
Dani <daniofer0@gmail.com>
2025-04-28 09:27:53 +0200

    Improved begincutscene

--- Assets/MugenRailroad/Misc/LiftPlatform/BeginCutscene.cs
+++ Assets/MugenRailroad/Misc/LiftPlatform/BeginCutscene.cs
@@ -10,9 +10,22 @@ public class BeginCutscene : MonoBehaviour
     private Animator elevatorAnimator;
     private GameObject cutsceneCamera;
     private Animator cutsceneAnimator;
+    private GameObject player;
+    private GameObject walkie;
+    private Animator walkieAnimator;
 
     private void Awake()
     {
+        walkie = GameObject.Find("WalkieCutscene");
+
+        if (walkie)
+        {
+            walkieAnimator = walkie.GetComponent<Animator>();
+            walkie.SetActive(false);
+        }
+        else
+            Debug.LogWarning("No se ha encontrado el walkie de la cinematica del ascensor en al escena.");
+        
         elevatorAnimator = GetComponent<Animator>();
         
         cutsceneCamera = GameObject.Find("BeginCutsceneCamera");
@@ -29,12 +42,15 @@ public class BeginCutscene : MonoBehaviour
     {
         if (obj.CompareTag("Player") && !cameraTriggered)
         {
+            player = obj.gameObject;
             Invoke("Begin", 0.3f);
         }
     }
 
     private void Begin()
     {
+        player.transform.position = new Vector3(-9.89000416f, 0.156999946f, 42.3300018f);
+        player.SetActive(false);
         cutsceneCamera.SetActive(true);
         cutsceneAnimator.SetTrigger("Enable");
         StartCoroutine(EsperarFinAnimacion());
@@ -48,14 +64,19 @@ public class BeginCutscene : MonoBehaviour
         
         AnimatorStateInfo stateInfo = cutsceneAnimator.GetCurrentAnimatorStateInfo(0);
         
+        walkie.SetActive(true);
+        
         while (!stateInfo.IsName("End"))
         {
             yield return null;
             stateInfo = cutsceneAnimator.GetCurrentAnimatorStateInfo(0);
         }
 
+        
+        
         cutsceneAnimator.enabled = false;
         elevatorAnimator.SetTrigger("Enable");
+        walkieAnimator.SetTrigger("Enable");
         elevatorTriggered = true;
         
         StartCoroutine(EsperarFinElevator());
@@ -73,6 +94,17 @@ public class BeginCutscene : MonoBehaviour
             stateInfo = elevatorAnimator.GetCurrentAnimatorStateInfo(0);
         }
         
+        walkieAnimator.SetTrigger("End");
+        
+        stateInfo = walkieAnimator.GetCurrentAnimatorStateInfo(0);
+        while (!stateInfo.IsName("End") || stateInfo.normalizedTime < 1f)
+        {
+            yield return null;
+            stateInfo = walkieAnimator.GetCurrentAnimatorStateInfo(0);
+        }
+        
+        walkie.SetActive(false);
+        player.SetActive(true);
         CutsceneCamera.EndCutscene();
         done = true;
     }
